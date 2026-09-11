7316c83b1b561953ea130e3955b7aa6bcc50cd1b
rodrigomgrassioto <rodrigomgrassioto@gmail.com>
2026-09-11 18:28:36 -0400

    refactor: retira o any na variável err

--- src/middlewares/errorMiddleware.ts
+++ src/middlewares/errorMiddleware.ts
@@ -1,29 +1,35 @@
 import { Request, Response, NextFunction } from 'express';
 
 export default function errorMiddleware(
-  err: any,
-  req: Request,
-  res: Response,
-  next: NextFunction
+    err: unknown,
+    _req: Request,
+    res: Response,
+    _next: NextFunction
 ): void {
   let status = 500;
   let message = 'Erro interno no servidor';
 
-  if (err.message === "Campos nome, e-mail e senha são obrigatórios") {
-    status = 400;
-    message = err.message;
-  } else if (err.message === "Formato de e-mail inválido") {
-    status = 400;
-    message = err.message;
-  } else if (err.message === "E-mail ou senha inválidos") {
-    status = 401;
-    message = err.message;
-  } else if (err.message === "Token inválido ou expirado") {
-    status = 401;
-    message = err.message;
-  } else if (err.message === "E-mail já cadastrado") {
-    status = 409;
-    message = err.message;
+  // Se err for uma instância da classe Error
+  if (err instanceof Error) {
+    if (err.message === "Campos nome, e-mail e senha são obrigatórios") {
+      status = 400;
+      message = err.message;
+    } else if (err.message === "Formato de e-mail inválido") {
+      status = 400;
+      message = err.message;
+    } else if (err.message === "E-mail ou senha inválidos") {
+      status = 401;
+      message = err.message;
+    } else if (err.message === "Token inválido ou expirado") {
+      status = 401;
+      message = err.message;
+    } else if (err.message === "E-mail já cadastrado") {
+      status = 409;
+      message = err.message;
+    }
+  } else if (typeof err === 'string') {
+    // Segurança extra: se alguém der um throw "Mensagem em Texto Puro"
+    message = err;
   }
 
   res.status(status).json({ success: false, message });
