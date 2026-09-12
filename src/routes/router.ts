dff7fb2c271b1be51cd22f4e074f089e93a701d9
rodrigomgrassioto <rodrigomgrassioto@gmail.com>
2026-09-12 11:11:01 -0400

    refactor: retira função da rota e move para UserController.ts

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -2,10 +2,12 @@ import { Router } from 'express';
 import { AuthController } from '../controllers/AuthController';
 import { authMiddleware } from '../middlewares/authMiddleware';
 import { roleMiddleware } from '../middlewares/roleMiddleware';
+import {UserController} from "../controllers/UserController";
 
 const router = Router();
 
 const authController = new AuthController();
+const userController = new UserController();
 
 
 // Rota pública: Registro de usuário
@@ -15,18 +17,9 @@ router.post('/auth/register', authController.register);
 router.post('/auth/login', authController.login);
 
 // Rota protegida: Informações do usuário autenticado
-router.get('/users/me', authMiddleware, (req, res) => {
-  res.status(200).json({ success: true, user: req.user });
-});
+router.get('/users/me', authMiddleware, userController.me);
 
 // Rota protegida por perfil: Ping para administradores
-router.get(
-  '/admin/ping',
-  authMiddleware,
-  roleMiddleware(['ADMIN']),
-  (req, res) => {
-    res.status(200).json({ message: "Pong! Acesso administrativo confirmado." });
-  }
-);
+router.get('/admin/ping', authMiddleware, roleMiddleware(['ADMIN']), userController.adminPing);
 
 export default router;
