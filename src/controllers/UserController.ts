9aacb1ed1bf0ab2836d4c1b6fdcef8b568454765
rodrigomgrassioto <rodrigomgrassioto@gmail.com>
2026-09-12 14:38:39 -0400

    clear: retira const de debug

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -2,8 +2,6 @@ import { Request, Response } from 'express';
 
 export class UserController {
     me(req: Request, res: Response): void {
-        const tst = req
-        const ps = "passou";
         res.status(200).json({
             success: true,
             user: req.user
